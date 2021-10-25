f486043179d36bb68b94860780ebb5d931e2fc0d
Luis Sandoval <luisasandovalcoder@gmail.com>
2021-10-24 19:49:55 -0600

    Initial Commit

---
+++ platzi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  platzi
+//
+//  Created by Luis Sandoval on 24/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ platzi/platziApp.swift
@@ -0,0 +1,17 @@
+//
+//  platziApp.swift
+//  platzi
+//
+//  Created by Luis Sandoval on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct platziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
