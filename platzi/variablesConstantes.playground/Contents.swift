e8bec76d41bc282f0371a24830005bc8758929b6
Luis Sandoval <luisasandovalcoder@gmail.com>
2021-10-25 19:11:16 -0600

    this was the class number 4 of the scratch swift course from Platzi

---
+++ platzi/variablesConstantes.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+//pr - nombre - tipo - oa - valor
+var numeroEntero:Int = 2
+
+var numeroFlotante:Float = 16.8
+
+var resultado = Float(numeroEntero) + numeroFlotante
+
+var datoString:String = ""
+var datoBooleano:Bool = true
+
+print(resultado)
+
+
+
